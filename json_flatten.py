b8f278566c20ad775681caf03069c3fc04b00922
Colin B <lanzaa@gmail.com>
2012-10-24 14:09:39 -0700

    put json faltten into try catch blocks

--- json_flatten.py
+++ json_flatten.py
@@ -1,23 +1,26 @@
-import ast
-import time
+#!/usr/bin/python2
 import sys
 import json
 
 print "date,venue-id,venue-name,mayor-type,user-gender,geolat,geolong,timezone,pc-id,pc-fullname,pc-nodename,pc-iconurl"
 for line in sys.stdin:
-    dline = json.loads(line)['checkin']
-    dout = []
-    dout.append(str(dline['created']))
-    dout.append(dline['venue']['id'])
-    dout.append(dline['venue']['name'])
-    dout.append(dline['mayor']['type'])
-    dout.append(dline['user']['gender'])
-    dout.append(str(dline['geolat']))
-    dout.append(str(dline['geolong']))
-    dout.append(dline['timezone'])
-    dout.append(dline['primarycategory']['id'])
-    dout.append(dline['primarycategory']['fullpathname'])
-    dout.append(dline['primarycategory']['nodename'])
-    dout.append(dline['primarycategory']['iconurl'])
-    print '"'+'","'.join(dout)+'"'
+    try:
+        dline = json.loads(line)['checkin']
+        dout = []
+        dout.append(str(dline['created']))
+        dout.append(dline['venue']['id'])
+        dout.append(dline['venue']['name'])
+        dout.append(dline['mayor']['type'])
+        dout.append(dline['user']['gender'])
+        dout.append(str(dline['geolat']))
+        dout.append(str(dline['geolong']))
+        dout.append(dline['timezone'])
+        dout.append(dline['primarycategory'].get('id','-'))
+        dout.append(dline['primarycategory'].get('fullpathname','-'))
+        dout.append(dline['primarycategory'].get('nodename','-'))
+        dout.append(dline['primarycategory'].get('iconurl','-'))
+        print '"'+'","'.join(dout)+'"'
+    except:
+        # Skip this line, something is broken
+        print >> sys.stderr, line,
 
